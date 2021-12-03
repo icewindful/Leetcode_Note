5205e47b4ea68c983a8128f54bbec386e161b47e
icewindful <icewindful@gmail.com>
2021-12-03 17:40:28 +0800

    Create 01_two_sum_01.py
    #Time complexity O(n^2)
    #Space complexity O(1)
    
    base method can use any process

---
+++ easy/01_two_sum_01.py
@@ -0,0 +1,10 @@
+#Time complexity O(n^2)
+#Space complexity O(1)
+#base method on any process.
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[j] == target - nums[i]:
+                    return [i, j]
