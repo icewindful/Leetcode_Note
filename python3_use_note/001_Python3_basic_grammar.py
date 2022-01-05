f2a4b5549e7d7b56f2f4a26115ac4695617e4cde
icewindful <icewindful@gmail.com>
2022-01-06 02:42:54 +0800

    Add files via upload
    002 many base use note.

---
+++ python3_use_note/001_Python3_basic_grammar.py
@@ -0,0 +1,17 @@
+#Python3 basic grammar
+
+#multi-line
+
+item_one ,item_two ,item_three = 1,2,3
+
+total = item_one + \
+        item_two + \
+        item_three 
+
+#PS: if linklist , Tuple , Sets , Dictionary . usen't it.
+
+input("\n\n input enter exit!!!")
+
+import sys; x = 'w3big'; sys.stdout.write(x + '\n')
+
+
