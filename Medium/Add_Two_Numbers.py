f7e177eebcdd57f69cfe16823889a8ad2ed971f2
icewindful <icewindful@gmail.com>
2022-03-16 19:35:08 +0800

    Add files via upload
    update New methods

--- Medium/Add_Two_Numbers.py
+++ Medium/Add_Two_Numbers.py
@@ -1,27 +1,44 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-### analysis ###
-# math method , before next->node save carry value.
-# item top pull . val / 10 get carry val , and push to next node.
-###   end    ###
-class Solution:
-    def addTwoNumbers(self, linklist1, linklist2 ,carry = 0):
-        """
-        :type linklist1: ListNode
-        :type linklist2: ListNode
-        :rtype: ListNode
-        """
-        val = linklist1.val + linklist2.val + carry
-        carry = val // 10
-        ret = ListNode(val % 10 ) 
+#Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution(object):
+    def addTwoNumbers(self, l1:ListNode , l2:ListNode ):
         
-        if (linklist1.next != None or linklist2.next != None or carry != 0):
-            if linklist1.next == None:
-                linklist1.next = ListNode(0)
-            if linklist2.next == None:
-                linklist2.next = ListNode(0)
-            ret.next = self.addTwoNumbers(linklist1.next,linklist2.next,carry)
-        return ret
+        result = ListNode(0)
+        result_tail = result
+        carry = 0
+                
+        while l1 or l2 or carry:            
+            val1  = (l1.val if l1 else 0)
+            print(val1)
+            val2  = (l2.val if l2 else 0)
+            print(val2)
+            carry, out = divmod(val1+val2 + carry, 10)
+            #print(carry,out)
+            """
+            divmod : example number , 11 : out = 15/10 (get last) = 5 / carry = 15/10 ( get first ) = 1
+            """          
+            result_tail.next = ListNode(out)
+            result_tail = result_tail.next                      
+            
+            l1 = (l1.next if l1 else None)
+            l2 = (l2.next if l2 else None)
+               
+        return result.next
+
+
+l1 = ListNode(2)
+l1.next = ListNode(4)
+l1.next.next = ListNode(3)
+
+l2 = ListNode(5)
+l2.next = ListNode(6)
+l2.next.next = ListNode(4)
+
+l3 = ListNode(0)
+
+Solution.addTwoNumbers(l3,l1,l2)
+    
